d51ad8dd0b7f91b1db13877dec8980a037b379ce
David Rose <doppler@gmail.com>
2019-02-22 16:43:41 -0500

    dunno. something graphy

--- src/App.js
+++ src/App.js
@@ -1,28 +1,76 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const screenDimensions = () => ({
+  width: window.innerWidth,
+  height: window.innerHeight
+});
 
+const GridSpacing = 50;
+
+const Grid = props => {
+  const { width, height } = props.dimensions;
+  const center = { x: width / 2, y: height / 2 };
+  let x = 0 - GridSpacing + (center.x % GridSpacing);
+  let y = 0 - GridSpacing + (center.y % GridSpacing);
+  return (
+    <g id="grid">
+      {Array.from({ length: Math.ceil(width / GridSpacing) }).map((_, i) => {
+        x = x + GridSpacing;
+        return (
+          <line key={i} className="gline" x1={x} y1={0} x2={x} y2={height} />
+        );
+      })}
+      {Array.from({ length: Math.ceil(height / GridSpacing) }).map((_, i) => {
+        y = y + GridSpacing;
+        return (
+          <line className="gline" key={i} x1={0} y1={y} x2={width} y2={y} />
+        );
+      })}
+    </g>
+  );
+};
+
+const App = () => {
+  const [dimensions, setDimensions] = useState(screenDimensions());
+  const { width, height } = dimensions;
+  const center = { x: width / 2, y: height / 2 };
+  let x = 0 - GridSpacing + (center.x % GridSpacing);
+  // let y = 0 - GridSpacing + (center.y % GridSpacing);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setDimensions(screenDimensions());
+    });
+  }, []);
+
+  return (
+    <div id="App">
+      <svg width={width} height={height}>
+        <Grid dimensions={dimensions} />
+        {Array.from({ length: Math.ceil((width * GridSpacing) / 4) }).map(
+          (_, i) => {
+            x = x + GridSpacing / 4;
+            return (
+              <>
+                <circle
+                  cx={x}
+                  cy={center.y + (height / 2) * Math.cos(x)}
+                  r={5}
+                  style={{ stroke: "hsl(120, 100%, 50%" }}
+                />
+                <circle
+                  cx={x}
+                  cy={center.y + (height / 2) * Math.cos(x * 2)}
+                  r={5}
+                  style={{ stroke: "hsl(120, 100%, 50%" }}
+                />
+              </>
+            );
+          }
+        )}
+      </svg>
+    </div>
+  );
+};
 export default App;
