3b24fe018bfe27e9d6a4cd29a6baca7e6c49244a
David Rose <doppler@gmail.com>
2019-02-22 22:20:37 -0500

    Updates

---
+++ precache-manifest.804b499265a2315ad1277920d17be077.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb2b1da3c31cbf00bfff",
+    "url": "/waves/static/css/main.d461627b.chunk.css"
+  },
+  {
+    "revision": "fb2b1da3c31cbf00bfff",
+    "url": "/waves/static/js/main.fb2b1da3.chunk.js"
+  },
+  {
+    "revision": "870301d9b4c5a1958763",
+    "url": "/waves/static/js/runtime~main.870301d9.js"
+  },
+  {
+    "revision": "57a5c7597d165803f7aa",
+    "url": "/waves/static/js/2.57a5c759.chunk.js"
+  },
+  {
+    "revision": "db0bbe693f9fd264b1f53f8ed09de0d5",
+    "url": "/waves/index.html"
+  }
+];
